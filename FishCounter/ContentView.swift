ae510e9832d2f4a83c0571dbbeb5b43e1f85b6f2
Minwoo Lee <minwoolee@koreanair.com>
2023-11-07 09:27:45 +0900

    Initial Commit

---
+++ FishCounter/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FishCounter
+//
+//  Created by Minwoo Lee on 11/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FishCounter/FishCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishCounterApp.swift
+//  FishCounter
+//
+//  Created by Minwoo Lee on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FishCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
